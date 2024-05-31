11cad908a21bcfe863801e113693096d0ba706b2
Crstian Ojeda <210180@utxicotepec.edu.mx>
2024-05-31 14:20:08 -0600

    Creacion de la vista sidebar

--- frontend/sistema-hospital/src/components/login.vue
+++ frontend/sistema-hospital/src/components/login.vue
@@ -1,26 +1,70 @@
 <template>
-<main class="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white">
-    <section class="flex w-[30rem] flex-col space-y-10">
-        <div class="text-center text-4xl font-medium">Inicio de sesion</div>
-
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="text" placeholder="Correo electronico" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
+    <div class="min-h-screen bg-lime-200 flex flex-col justify-center py-12 sm:px-6 lg:px-8 px-6">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <img class="mx-auto h-10 w-auto" src="https://www.svgrepo.com/show/301692/login.svg" alt="Workflow">
+            <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+                Iniciar Sesion
+            </h2>
         </div>
 
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="password" placeholder="Contraseña" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
+
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form>
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-5  text-gray-700">Correo
+                            electronico</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="email" name="email" placeholder="user@example.com" type="email"  value=""
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+                            <div class="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                                <svg class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-6">
+                        <label for="password" class="block text-sm font-medium leading-5 text-gray-700">Contraseña</label>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <input id="password" name="password" type="password" 
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+                        </div>
+                    </div>
+
+                    <div class="mt-6 flex items-center justify-between">
+                        <div class="flex items-center">
+                            <input id="remember_me" name="remember" type="checkbox" value="1"
+                                class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out">
+                            <label for="remember_me" class="ml-2 block text-sm leading-5 text-gray-900">Recordar sesion</label>
+                        </div>
+
+                    </div>
+
+                    <div class="mt-6">
+                        <span class="block w-full rounded-md shadow-sm">
+
+                            <a href="/home"
+                            class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                                Iniciar
+                            </a>
+                         
+                        </span>
+                    </div>
+                    <div class="mt-6 flex items-center justify-center">
+                        <div class="text-sm leading-5">
+                            <a href="/register"
+                                class="font-medium text-blue-500 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                                Registrarse
+                            </a>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
         </div>
-
-        <button class="transform rounded-sm bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-400">Iniciar</button>
-
-        <a href="#" class="transform text-center font-semibold text-gray-500 duration-300 hover:text-gray-300">FORGOT PASSWORD?</a>
-
-        <p class="text-center text-lg">
-            No tiene cuenta?
-            <a href="register" class="font-medium text-indigo-500 underline-offset-4 hover:underline">Crear cuenta</a>
-        </p>
-    </section>
-</main>
-
-    
+    </div>
 </template>
--- frontend/sistema-hospital/src/components/registerUser.vue
+++ frontend/sistema-hospital/src/components/registerUser.vue
@@ -1,53 +1,138 @@
 <template>
-    <main class="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white">
-    <section class="flex w-[30rem] flex-col space-y-10">
-        <div class="text-center text-4xl font-medium">Registro</div>
-
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="text" placeholder="Nombre" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
+    <div class="min-h-screen bg-lime-200 flex flex-col justify-center py-5 sm:px-6 lg:px-8 px-6">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <img class="mx-auto h-10 w-auto" src="https://www.svgrepo.com/show/301692/login.svg" alt="Workflow">
+            <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+                Registro
+            </h2>
         </div>
 
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="text" placeholder="Primer apellido" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
+
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form>
+                    <div>
+                        <label for="text" class="block text-sm font-medium leading-5  text-gray-700">Nombre</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="name" name="name" type="text" required value=""
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+
+                        </div>
+                    </div>
+                    <div>
+                        <label for="text" class="block text-sm font-medium leading-5  text-gray-700">Primer apellido</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="primer_apellido" name="name" type="text" required value=""
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+
+                        </div>
+                    </div>
+                    <div>
+                        <label for="text" class="block text-sm font-medium leading-5  text-gray-700">Segundo
+                            apellido</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="segundo_apellido" name="name" type="text" required value=""
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+
+                        </div>
+                    </div>
+                    <div>
+                        <label for="text" class="block text-sm font-medium leading-5  text-gray-700">CURP</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="curp" name="name" type="text" required value=""
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+
+                        </div>
+                    </div>
+                    <div>
+                        <label for="genero" class="block text-sm font-medium leading-5 text-gray-700">Género</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <select id="genero" name="genero" required
+                                class="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+                                <option value="" disabled selected>Selecciona un género</option>
+                                <option value="hombre">Hombre</option>
+                                <option value="mujer">Mujer</option>
+                                <option value="otro">Otro</option>
+                                <!-- Agrega más opciones según tus necesidades -->
+                            </select>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label for="genero" class="block text-sm font-medium leading-5 text-gray-700">Tipo de sangre</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <select id="genero" name="genero" required
+                                class="block w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+                                <option value="" disabled selected>Selecciona un grupo</option>
+                                <option value="A+">A+</option>
+                                <option value="A-">A-</option>
+                                <option value="B+">B+</option>
+                                <option value="B-">B-</option>
+                                <option value="AB+">AB+</option>
+                                <option value="AB-">AB-</option>
+                                <option value="O+">O+</option>
+                                <option value="O-">O-</option>
+                                <!-- Agrega más opciones según tus necesidades -->
+                            </select>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="text" class="block text-sm font-medium leading-5  text-gray-700">Fecha de
+                            nacimiento</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="fecha_nacimiento" name="name" type="date" required value=""
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+
+                        </div>
+                    </div>
+                    <div>
+                        <label for="text" class="block text-sm font-medium leading-5  text-gray-700">Telefono</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="name" name="name" type="tel" required value=""
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+
+                        </div>
+                    </div>
+
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-5  text-gray-700">Correo
+                            electronico</label>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                            <input id="email" name="email" placeholder="user@example.com" type="email" required value=""
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+
+                        </div>
+                    </div>
+
+                    <div>
+                        <label for="password" class="block text-sm font-medium leading-5 text-gray-700">Contraseña</label>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <input id="password" name="password" type="password" required
+                                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+                        </div>
+                    </div>
+
+
+                    <div class="mt-6">
+                        <span class="block w-full rounded-md shadow-sm">
+                            <button type="submit"
+                                class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                                Iniciar
+                            </button>
+                        </span>
+                    </div>
+
+                    <div class="mt-6 flex items-center justify-center">
+                        <div class="text-sm leading-5">
+                            <a href="/"
+                                class="font-medium text-blue-500 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                                Iniciar Sesion
+                            </a>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
         </div>
-
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="text" placeholder="Segundo apellido" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
-        </div>
-
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="text" placeholder="Edad" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
-        </div>
-
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="text" placeholder="Fecha de nacimiento" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
-        </div>
-
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="text" placeholder="Correo electronico" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
-        </div>
-
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="text" placeholder="Telefono" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
-        </div>
-
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="password" placeholder="Contraseña" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
-        </div>
-
-        <div class="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
-            <input type="password" placeholder="Confirmar contraseña" class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"/>
-        </div>
-
-        <button class="transform rounded-sm bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-400">Registrar</button>
-
-        <a href="#" class="transform text-center font-semibold text-gray-500 duration-300 hover:text-gray-300">FORGOT PASSWORD?</a>
-
-        <p class="text-center text-lg">
-            Ya tiene cuenta?
-            <a href="/" class="font-medium text-indigo-500 underline-offset-4 hover:underline">Iniciar Sesion</a>
-        </p>
-    </section>
-</main>
-
+    </div>
 </template>
