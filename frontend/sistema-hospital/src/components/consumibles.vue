7c5616ee75ec4a4ed8381b138ecf3020d66fd408
Daniela-AG15 <danaguilar1512@gmail.com>
2024-07-17 10:38:01 -0600

    Edicion de los Campos en el formulario en Consumibles

--- frontend/sistema-hospital/src/components/consumibles.vue
+++ frontend/sistema-hospital/src/components/consumibles.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="bg-gray-200 w-full h-full min-h-screen   ">
+  <div class="bg-gray-200 w-full h-full min-h-screen">
 
     <form>
 
-      <div class="mx-auto w-full lg:w-7/12  dark:bg-gray-700 p-8 lg:rounded-l-none">
+      <div class="mx-auto w-full lg:w-7/12 dark:bg-gray-700 p-8 lg:rounded-l-none">
         <h3 class="py-4 text-3xl font-semibold text-center text-gray-800 dark:text-white">Dispensacion</h3>
         <form class="px-8 pt-6 pb-8 mb-4 bg-white dark:bg-gray-800 rounded">
           <a href="/tablaDis"
@@ -14,84 +14,92 @@
           <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
 
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="nombre">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Nombre">
                 Nombre
               </label>
               <input
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="nombre" type="text" placeholder="Nombre" />
+                id="Nombre" type="text" placeholder="Nombre" />
             </div>
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="descripcion">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Descripcion">
                 Descripción
               </label>
               <input
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="descripcion" type="text" placeholder="Descripción" />
+                id="Descripcion" type="text" placeholder="Descripción" />
             </div>
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="tipo">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Tipo">
                 Tipo
               </label>
               <input
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="tipo" type="text" placeholder="Tipo" />
+                id="Tipo" type="text" placeholder="Tipo" />
             </div>
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="departamento">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Departamento">
                 Departamento
               </label>
               <input
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="departamento" type="text" placeholder="Departamento" />
+                id="Departamento" type="text" placeholder="Departamento" />
             </div>
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="cantidad_existencia">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Cantidad_existencia">
                 Cantidad en Existencia
               </label>
               <input
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="cantidad_existencia" type="number" placeholder="Cantidad en Existencia" />
+                id="Cantidad_existencia" type="number" placeholder="Cantidad en Existencia" />
             </div>
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="detalle">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Detalle">
                 Detalle
               </label>
               <textarea
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="detalle" rows="3" placeholder="Detalle"></textarea>
+                id="Detalle" rows="3" placeholder="Detalle"></textarea>
             </div>
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="fecha_registro">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Fecha_registro">
                 Fecha de Registro
               </label>
               <input
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="fecha_registro" type="date" />
+                id="Fecha_registro" type="date" />
             </div>
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="estatus">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Fecha_actualizacion">
+                Fecha de Actualización
+              </label>
+              <input
+                class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
+                id="Fecha_actualizacion" type="date" />
+            </div>
+            <div>
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Estatus">
                 Estatus
               </label>
               <input
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="estatus" type="text" placeholder="Estatus" />
+                id="Estatus" type="text" placeholder="Estatus" />
             </div>
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="observaciones">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Observaciones">
                 Observaciones
               </label>
               <textarea
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="observaciones" rows="3" placeholder="Observaciones"></textarea>
+                id="Observaciones" rows="3" placeholder="Observaciones"></textarea>
             </div>
             <div>
-              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="espacio_medico">
+              <label class="block mb-2 text-sm font-semibold text-gray-700 dark:text-white" for="Espacio_medico">
                 Espacio Médico
               </label>
               <input
                 class="w-full px-4 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:text-white dark:bg-gray-700 dark:border-gray-600"
-                id="espacio_medico" type="text" placeholder="Espacio Médico" />
+                id="Espacio_medico" type="text" placeholder="Espacio Médico" />
             </div>
           </div>
           <div class="mt-6 text-center">
@@ -112,4 +120,3 @@
     </form>
   </div>
 </template>
-  
